40081fdb0f6d69fff769087522ea70fa28f05e38
eugenelijondo <eugene.lijondo@student.moringaschool.com>
2024-02-11 19:57:44 +0300

    random dogs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImageUrl, setDogImageUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        if (!response.ok) {
+          throw new Error('Failed to fetch dog image');
+        }
+        const data = await response.json();
+        setDogImageUrl(data.message);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImageUrl} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
